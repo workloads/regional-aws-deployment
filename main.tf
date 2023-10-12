66e264f243861d92569b6797c245ca06be5a921a
Kerim Satirli <k.satirli@gmail.com>
2023-10-12 22:20:20 +0200

    cleanup

--- main.tf
+++ main.tf
@@ -29,33 +29,7 @@ locals {
     datacenter = var.aws_region
 
     join_tags = [
-      # primary cluster
       "provider=aws region=${var.aws_region} tag_key=nomad:role tag_value=server addr_type=public_v4",
-
-      # alternate regions, for federation
-      # TODO: update discovery to be more programmatic
-      "provider=aws region=af-south-1 tag_key=nomad:role tag_value=server addr_type=public_v4",
-      "provider=aws region=ap-east-1 tag_key=nomad:role tag_value=server addr_type=public_v4",
-      "provider=aws region=ap-northeast-1 tag_key=nomad:role tag_value=server addr_type=public_v4",
-      "provider=aws region=ap-northeast-2 tag_key=nomad:role tag_value=server addr_type=public_v4",
-      "provider=aws region=ap-northeast-3 tag_key=nomad:role tag_value=server addr_type=public_v4",
-      "provider=aws region=ap-south-1 tag_key=nomad:role tag_value=server addr_type=public_v4",
-      "provider=aws region=ap-southeast-1 tag_key=nomad:role tag_value=server addr_type=public_v4",
-      "provider=aws region=ap-southeast-2 tag_key=nomad:role tag_value=server addr_type=public_v4",
-      "provider=aws region=ap-southeast-3 tag_key=nomad:role tag_value=server addr_type=public_v4",
-      "provider=aws region=ca-central-1 tag_key=nomad:role tag_value=server addr_type=public_v4",
-      "provider=aws region=eu-central-1 tag_key=nomad:role tag_value=server addr_type=public_v4",
-      "provider=aws region=eu-north-1 tag_key=nomad:role tag_value=server addr_type=public_v4",
-      "provider=aws region=eu-south-1 tag_key=nomad:role tag_value=server addr_type=public_v4",
-      "provider=aws region=eu-west-1 tag_key=nomad:role tag_value=server addr_type=public_v4",
-      "provider=aws region=eu-west-2 tag_key=nomad:role tag_value=server addr_type=public_v4",
-      "provider=aws region=eu-west-3 tag_key=nomad:role tag_value=server addr_type=public_v4",
-      "provider=aws region=me-south-1 tag_key=nomad:role tag_value=server addr_type=public_v4",
-      "provider=aws region=sa-east-1 tag_key=nomad:role tag_value=server addr_type=public_v4",
-      "provider=aws region=us-east-1 tag_key=nomad:role tag_value=server addr_type=public_v4",
-      "provider=aws region=us-east-2 tag_key=nomad:role tag_value=server addr_type=public_v4",
-      "provider=aws region=us-west-1 tag_key=nomad:role tag_value=server addr_type=public_v4",
-      "provider=aws region=us-west-2 tag_key=nomad:role tag_value=server addr_type=public_v4",
     ]
 
     region = "aws"
