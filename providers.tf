c77ed567c1ea0e19bc42a62ced5e9407a139f196
Kerim Satirli <k.satirli@gmail.com>
2024-07-28 11:41:56 +0200

    linting

--- providers.tf
+++ providers.tf
@@ -5,9 +5,9 @@ provider "aws" {
   # see https://registry.terraform.io/providers/hashicorp/aws/latest/docs#default_tags
   default_tags {
     tags = {
-      "github:url"                   = "https://github.com/workloads/regional-aws-deployment"
-      "terraform-cloud:organization" = var.tfe_organization
-      "terraform-cloud:workspace"    = var.tfe_workspace
+      "github:url"                 = "https://github.com/workloads/regional-aws-deployment"
+      "hcp-terraform:organization" = var.tfe_organization
+      "hcp-terraform:workspace"    = var.tfe_workspace
     }
   }
 }
